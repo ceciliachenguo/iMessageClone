cdb89b86760c3054ca803ba22009959fd48b262c
CeciliaChen <cecilia@pdotf.com>
2023-08-20 11:27:03 -0400

    Initial Commit

---
+++ iMessageClone/iMessageCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageCloneApp.swift
+//  iMessageClone
+//
+//  Created by Cecilia Chen on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iMessageCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
